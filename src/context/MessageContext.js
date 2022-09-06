22541e33e4b7aeeba23d7c217f40946f323c7c9b
Hassan Ayomide <funyem1710@gmail.com>
2022-09-06 02:14:10 +0100

    implementing context for nessages

--- src/context/MessageContext.js
+++ src/context/MessageContext.js
@@ -1,24 +1,91 @@
-import { collection, getDocs, limit, orderBy, query } from 'firebase/firestore'
-import React, { useContext, useState } from 'react'
-import { db } from '../firebase/firebase'
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  Timestamp,
+  where,
+} from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { auth, db } from "../firebase/firebase";
 
-const MessageContext = React.createContext()
-export default function MessageProvider({children}) {
-    const [message, setMessage] = useState('')
-    async function chatRoom(){
-        const texts = query(collection(db, 'message'), orderBy('createdAt'), limit(25))
-        const querySnapshot = await getDocs(texts)
-    }
-    const value = {
-        message,
-        setMessage
-    }
+const MessageContext = React.createContext();
+export default function MessageProvider({ children }) {
+  const [users, setUsers] = useState([]);
+  const [chat, setChat] = useState("");
+  const [text, setText] = useState("");
+  const [messages, setMessages] = useState([]);
+  //id of sender
+  const senderId = auth.currentUser.uid;
+  //get all users
+  useEffect(() => {
+    const usersRef = collection(db, "users");
+    // get query of all users except the current logged in user
+    const getAllUsers = query(usersRef, where("uid", "not-in", [senderId]));
+    const unsubscribe = onSnapshot(getAllUsers, (snapshot) => {
+      let usersArr = [];
+      snapshot.forEach((doc) => {
+        usersArr.push(doc.data());
+      });
+      setUsers(usersArr);
+    });
+    return () => unsubscribe();
+  }, [senderId]);
+
+  // select a user and get the messages between the sender and receiver
+  function selectUser(user) {
+    console.log(user);
+    setChat(user);
+    const receiverId = user.uid;
+    const id =
+      senderId > receiverId
+        ? `${senderId + receiverId}`
+        : `${receiverId + senderId}`;
+    const messageRef = collection(db, "messages", id, "chat");
+    const messageSnapshot = query(messageRef, orderBy("createdAt", "asc"));
+    onSnapshot(messageSnapshot, (querySnapshot) => {
+      let msgs = [];
+      querySnapshot.forEach((doc) => {
+        msgs.push(doc.data());
+      });
+      setMessages(msgs);
+    });
+  }
+
+  // send message to receiver
+  async function sendMessage(e) {
+    console.log("sent");
+    e.preventDefault();
+    const receiverId = chat.uid;
+    const id =
+      senderId > receiverId
+        ? `${senderId + receiverId}`
+        : `${receiverId + senderId}`;
+    const chatRef = collection(db, "messages", id, "chat");
+    await addDoc(chatRef, {
+      text,
+      from: senderId,
+      to: receiverId,
+      createdAt: Timestamp.fromDate(new Date()),
+    });
+    setText("");
+  }
+  const value = {
+    users,
+    chat,
+    text,
+    messages,
+    senderId,
+    setMessages,
+    sendMessage,
+    selectUser,
+    setText
+  };
   return (
-    <MessageContext.Provider value={value}>
-        {children}
-    </MessageContext.Provider>
-  )
+    <MessageContext.Provider value={value}>{children}</MessageContext.Provider>
+  );
 }
-export function useMessages(){
-    return useContext(MessageContext)
+export function useMessages() {
+  return useContext(MessageContext);
 }
--- src/pages/ChatApp/ChatApp.jsx
+++ src/pages/ChatApp/ChatApp.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect, useRef } from "react";
 import ChatListTop from "../../components/ChatListTop/ChatListTop";
 import ChatScreen from "../../components/ChatScreen/ChatScreen";
 import InputBar from "../../components/InputBar/InputBar";
@@ -6,82 +5,9 @@ import TopBar from "../../components/TopBar/TopBar";
 import User from "../../components/User/User";
 import styles from "./chatApp.module.scss";
 import { LayoutWrapper } from "../../components/Layout/Layout";
-import {
-  addDoc,
-  collection,
-  onSnapshot,
-  orderBy,
-  query,
-  Timestamp,
-  where,
-} from "firebase/firestore";
-import { auth, db } from "../../firebase/firebase";
-import { useAuth } from "../../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { useMessages } from "../../context/MessageContext";
 export default function ChatApp() {
-  const [users, setUsers] = useState([]);
-  const [chat, setChat] = useState("");
-  const [text, setText] = useState("");
-  const [messages, setMessages] = useState([]);
-  const { logout } = useAuth();
-  const navigate = useNavigate();
-  const dummy = useRef()
-  //id of sender
-  const senderId = auth.currentUser.uid;
-  //get all users
-  useEffect(() => {
-    const usersRef = collection(db, "users");
-    // get query of all users except the current logged in user
-    const getAllUsers = query(usersRef, where("uid", "not-in", [senderId]));
-    const unsubscribe = onSnapshot(getAllUsers, (snapshot) => {
-      let usersArr = [];
-      snapshot.forEach((doc) => {
-        usersArr.push(doc.data());
-      });
-      setUsers(usersArr);
-    });
-    return () => unsubscribe();
-  }, [senderId]);
-
-  // select a user and get the messages between the sender and receiver
-  function selectUser(user) {
-    console.log(user);
-    setChat(user);
-    const receiverId = user.uid;
-    const id =
-      senderId > receiverId
-        ? `${senderId + receiverId}`
-        : `${receiverId + senderId}`;
-    const messageRef = collection(db, "messages", id, "chat");
-    const messageSnapshot = query(messageRef, orderBy("createdAt", "asc"));
-    onSnapshot(messageSnapshot, (querySnapshot) => {
-      let msgs = [];
-      querySnapshot.forEach((doc) => {
-        msgs.push(doc.data());
-      });
-      setMessages(msgs);
-    });
-  }
-
-  // send message to receiver
-  async function sendMessage(e) {
-    console.log("sent");
-    e.preventDefault();
-    const receiverId = chat.uid;
-    const id =
-      senderId > receiverId
-        ? `${senderId + receiverId}`
-        : `${receiverId + senderId}`;
-    const chatRef = collection(db, "messages", id, "chat");
-    await addDoc(chatRef, {
-      text,
-      from: senderId,
-      to: receiverId,
-      createdAt: Timestamp.fromDate(new Date()),
-    });
-    setText("");
-    
-  }
+  const {users, chat, selectUser, text, sendMessage, setText, messages, setMessages, senderId} = useMessages()
   
   return (
     <LayoutWrapper>
@@ -104,9 +30,6 @@ export default function ChatApp() {
                   setMessages={setMessages}
                 />
               </div>{" "}
-              <div ref={dummy}>
-
-              </div>
                 <InputBar
                   sendMessage={sendMessage}
                   text={text}
